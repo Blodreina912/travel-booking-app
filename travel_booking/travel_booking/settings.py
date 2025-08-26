6d03939bf1f94f8cbbbd065fd9669d24131f9a05
Blodreina912 <122105957+Blodreina912@users.noreply.github.com>
2025-08-26 09:18:14 +0000

    Describe what you changed

---
+++ travel_booking/travel_booking/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'travel_booking_db',
+        'USER': 'django_user',
+        'PASSWORD': 'strongpassword',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
